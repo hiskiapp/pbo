a4f9cac364f5ea0ed7a1f8e438218bb9d0bfb23a
Hiskia Pratama <hiskianggi@gmail.com>
2022-03-07 13:56:55 +0700

    feat: week 1

---
+++ week1/Introduction.java
@@ -0,0 +1,14 @@
+package week1;
+
+/**
+ * Introduction
+ */
+public class Introduction {
+    public static void main(String[] args) {
+        String name = "Hiskia Anggi Puji Pratama";
+
+        System.out.println("Halo, nama saya adalah " + name);
+        System.out.println("Saya adalah mahasiswa Teknik Informatika Universitas Dian Nuswantoro");
+        System.out.println("Saya sedang menulis program Java");
+    }
+}
